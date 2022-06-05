078eb777eca08568ff48cdc2864ccb30f68067fc
GMercury <kingorigithinji@gmail.com>
2022-06-05 10:32:40 +0300

    linked lists

--- 0x03-python-data_structures/linked_lists.c
+++ 0x03-python-data_structures/linked_lists.c
@@ -6,6 +6,8 @@
  * print_listint - prints all elements of a listint_t list
  * @h: pointer to head of list
  * Return: number of nodes
+ * Linked lists :-)_
+ *
  */
 size_t print_listint(const listint_t *h)
 {
