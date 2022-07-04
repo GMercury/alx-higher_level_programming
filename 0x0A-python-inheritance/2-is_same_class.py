13274dda61b7916b9869a8ba28c4c42cc1059dab
GMercury <99487197+GMercury@users.noreply.github.com>
2022-07-04 18:19:15 +0300

    0x0A python inheritance

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+    Module containing the `is_same_class` function.
+"""
+
+
+def is_same_class(obj, a_class):
+    """Check if `obj` is an exact instance of `a_class`.
+    Args:
+        obj: An object.
+        a_class: A class.
+    Returns: True if `obj` is an exact instance a `a_class`, otherwise False.
+    """
+    return (type(obj) is a_class)
---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+    Module containing the `BaseGeometry` class.
+"""
+
+
+class BaseGeometry:
+    """A class.
+    """
+    pass
