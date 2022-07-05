358f4217a40b314534235ae4d86a8c379561ba56
GMercury <kingorigithinji@gmail.com>
2022-07-05 19:15:07 +0300

    update 101-stats.py

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,39 +1,55 @@
 #!/usr/bin/python3
-""" Read lines from standard input and compute metrics
-Input Format:
-<IP Address> - [<date>] "GET /projects/260 HTTP/1.1" <status code> <file size>
+import sys
+
 """
+contains the "print_info" module
+"""
+def print_info():
+    """ script that reads stdin line by line and computes metrics"""
+    print('File size: {:d}'.format(file_size))
 
-from collections import defaultdict
-from sys import stdin, exit as sysexit
-
-STATUS_CODES = ['200', '301', '400', '401', '403', '404', '405', '500']
-
-
-def print_stats(file_size, status_codes):
-    print("File size: {}".format(total_size), *(
-        "{}: {}".format(k, status_codes[k]) for k in sorted(status_codes)
-    ), sep="\n")
+    for scode, code_times in sorted(status_codes.items()):
+        if code_times > 0:
+            print('{}: {:d}'.format(scode, code_times))
 
 
-if __name__ == '__main__':
-    status_codes = defaultdict(lambda: 0)
-    total_size = 0
-    line_count = 0
-    while True:
+status_codes = {
+    '200': 0,
+    '301': 0,
+    '400': 0,
+    '401': 0,
+    '403': 0,
+    '404': 0,
+    '405': 0,
+    '500': 0
+}
+
+lc = 0
+file_size = 0
+
+try:
+    for line in sys.stdin:
+        if lc != 0 and lc % 10 == 0:
+            print_info()
+
+        pieces = line.split()
+
         try:
-            for _ in range(10):
-                line = stdin.readline()
-                if line:
-                    *_, status_code, file_size = line.split()
-                    if status_code in STATUS_CODES:
-                        status_codes[status_code] += 1
-                    total_size += int(file_size)
-                else:
-                    if total_size:
-                        print_stats(file_size, status_codes)
-                    sysexit(0)
-            print_stats(file_size, status_codes)
-        except KeyboardInterrupt:
-            print_stats(file_size, status_codes)
-            raise
+            status = int(pieces[-2])
+
+            if str(status) in status_codes.keys():
+                status_codes[str(status)] += 1
+        except:
+            pass
+
+        try:
+            file_size += int(pieces[-1])
+        except:
+            pass
+
+        lc += 1
+
+    print_info()
+except KeyboardInterrupt:
+    print_info()
+    raise
