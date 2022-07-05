354e4d4d6077bb6fcf743164aa0446afdeec12da
GMercury <kingorigithinji@gmail.com>
2022-07-05 19:10:09 +0300

    update 101-stats.py

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,57 +1,39 @@
 #!/usr/bin/python3
-
-"""
-After every ten lines or the input of a keyboard interruption (CTRL + C),
-prints the following statistics:
-    - Total file size up to that point.
-    - Count of read status codes up to that point.
+""" Read lines from standard input and compute metrics
+Input Format:
+<IP Address> - [<date>] "GET /projects/260 HTTP/1.1" <status code> <file size>
 """
 
+from collections import defaultdict
+from sys import stdin, exit as sysexit
 
-def print_stats(size, status_codes):
-    """Print accumulated metrics.
-    Args:
-        size (int): The accumulated read file size.
-        status_codes (dict): The accumulated count of status codes.
-    """
-    print("File size: {}".format(size))
-    for key in sorted(status_codes):
-        print("{}: {}".format(key, status_codes[key]))
+STATUS_CODES = ['200', '301', '400', '401', '403', '404', '405', '500']
 
-if __name__ == "__main__":
-    import sys
 
-    size = 0
-    status_codes = {}
-    valid_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
-    count = 0
+def print_stats(file_size, status_codes):
+    print("File size: {}".format(total_size), *(
+        "{}: {}".format(k, status_codes[k]) for k in sorted(status_codes)
+    ), sep="\n")
 
-    try:
-        for line in sys.stdin:
-            if count == 10:
-                print_stats(size, status_codes)
-                count = 1
-            else:
-                count += 1
 
-            line = line.split()
-
-            try:
-                size += int(line[-1])
-            except (IndexError, ValueError):
-                pass
-
-            try:
-                if line[-2] in valid_codes:
-                    if status_codes.get(line[-2], -1) == -1:
-                        status_codes[line[-2]] = 1
-                    else:
-                        status_codes[line[-2]] += 1
-            except IndexError:
-                pass
-
-        print_stats(size, status_codes)
-
-    except KeyboardInterrupt:
-        print_stats(size, status_codes)
-        raise
+if __name__ == '__main__':
+    status_codes = defaultdict(lambda: 0)
+    total_size = 0
+    line_count = 0
+    while True:
+        try:
+            for _ in range(10):
+                line = stdin.readline()
+                if line:
+                    *_, status_code, file_size = line.split()
+                    if status_code in STATUS_CODES:
+                        status_codes[status_code] += 1
+                    total_size += int(file_size)
+                else:
+                    if total_size:
+                        print_stats(file_size, status_codes)
+                    sysexit(0)
+            print_stats(file_size, status_codes)
+        except KeyboardInterrupt:
+            print_stats(file_size, status_codes)
+            raise
