3a9f13c1fe3eb4e0552953b0b3f535acca641a78
GMercury <kingorigithinji@gmail.com>
2022-07-05 19:27:20 +0300

    update 101-stats.py

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,55 +1,43 @@
 #!/usr/bin/python3
+"""
+Take stdin and check the input, make some operation on it
+"""
 import sys
 
-"""
-contains the "print_info" module
-"""
-def print_info():
-    """ script that reads stdin line by line and computes metrics"""
-    print('File size: {:d}'.format(file_size))
-
-    for scode, code_times in sorted(status_codes.items()):
-        if code_times > 0:
-            print('{}: {:d}'.format(scode, code_times))
-
-
-status_codes = {
-    '200': 0,
-    '301': 0,
-    '400': 0,
-    '401': 0,
-    '403': 0,
-    '404': 0,
-    '405': 0,
-    '500': 0
+errorCode = {
+    "200": 0,
+    "301": 0,
+    "400": 0,
+    "401": 0,
+    "403": 0,
+    "404": 0,
+    "405": 0,
+    "500": 0,
 }
-
-lc = 0
-file_size = 0
+numOfLine = 0
+sumSize = 0
 
 try:
     for line in sys.stdin:
-        if lc != 0 and lc % 10 == 0:
-            print_info()
+        lineToken = line.split()
+        if len(lineToken) >= 2:
+            tmp = numOfLine
+            if lineToken[-2] in errorCode:
+                errorCode[lineToken[-2]] += 1
+            numOfLine += 1
+            sumSize += int(lineToken[-1])
+        if numOfLine % 10 == 0:
+            print("File size: {:d}".format(sumSize))
+            for key, value in sorted(errorCode.items()):
+                if value:
+                    print("{:s}: {:d}".format(key, value))
+    print("File size: {:d}".format(sumSize))
+    for key, value in sorted(errorCode.items()):
+        if value:
+            print("{:s}: {:d}".format(key, value))
 
-        pieces = line.split()
-
-        try:
-            status = int(pieces[-2])
-
-            if str(status) in status_codes.keys():
-                status_codes[str(status)] += 1
-        except:
-            pass
-
-        try:
-            file_size += int(pieces[-1])
-        except:
-            pass
-
-        lc += 1
-
-    print_info()
 except KeyboardInterrupt:
-    print_info()
-    raise
+    print("File size: {:d}".format(sumSize))
+    for key, value in sorted(errorCode.items()):
+        if value:
+            print("{:s}: {:d}".format(key, value))
